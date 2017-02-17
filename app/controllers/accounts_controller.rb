d7e78096d3967435cc35f65da91d9af49fb86a0e
Frank Olson <will.olson11@gmail.com>
2017-02-16 17:41:51 -0800

    add accounts and welcome controller and associated specs/routes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,9 +1,16 @@
 class AccountsController < ApplicationController
 
   def new
+    @account = Account.new
   end
 
   def create
+    @account = Account.new(account_params)
+    if @account.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   resources :accounts, only: [:new, :create]
 end
