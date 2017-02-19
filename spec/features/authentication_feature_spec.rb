1b3143151a472f1b5c2fc4f66bdd294bfd90da3b
Frank Olson <will.olson11@gmail.com>
2017-02-19 14:31:38 -0800

    update the authentication views

--- spec/features/authentication_feature_spec.rb
+++ spec/features/authentication_feature_spec.rb
@@ -5,17 +5,17 @@ describe 'user authentication' do
   let(:account) {create(:account, owner: user)}
 
   it 'allows signin with valid credentials' do
-    sign_user_in(user)
+    sign_user_in(account.owner)
     expect(page).to have_content('Signed in successfully')
   end
 
-  it 'does not allow signin with invalid credentials'do
-    sign_user_in(user, password: 'wrong_password')
+  it 'does not allow signin with invalid credentials' do
+    sign_user_in(account.owner, password: 'wrong_password')
     expect(page).to have_content('Invalid Email or password')
   end
 
   it 'allows user to sign out' do
-    sign_user_in(user)
+    sign_user_in(account.owner)
 
     click_link 'Sign out'
     expect(page).to have_content('Signed out successfully')
